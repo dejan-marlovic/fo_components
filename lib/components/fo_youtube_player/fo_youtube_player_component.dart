93641b12cc32c1db2bf79a90be1ae3ac2445caa7
Patrick Minogue <patrick.minogue@gmail.com>
2019-01-29 11:55:42 +0100

    Youtube component can now be scrolled over

--- lib/components/fo_youtube_player/fo_youtube_player_component.dart
+++ lib/components/fo_youtube_player/fo_youtube_player_component.dart
@@ -8,67 +8,71 @@ typedef void YoutubeCallback(JsObject event);
 @Component(
     selector: 'fo-youtube-player',
     styleUrls: const ['fo_youtube_player_component.css'],
-    templateUrl: 'fo_youtube_player_component.html'
-)
-class FoYouTubePlayerComponent implements AfterContentInit, OnChanges, OnDestroy
-{
+    templateUrl: 'fo_youtube_player_component.html')
+class FoYouTubePlayerComponent implements OnInit, OnChanges, OnDestroy {
   FoYouTubePlayerComponent();
 
-  @override
-  void ngAfterContentInit()
-  {
-    if (apiLoaded) throw new StateError('Only one fo-youtube-player can be created per app');
+  void onTouch() {
+    if (playing) {
+      _player.callMethod('pauseVideo');
+    } else {
+      _player.callMethod('playVideo');
+    }   
+  }
 
-    document.head.children.add(new ScriptElement()..src = 'https://www.youtube.com/iframe_api');
+  @override
+  void ngOnInit() {
+    if (apiLoaded)
+      throw new StateError('Only one fo-youtube-player can be created per app');
+
+    playing = autoplay;
+
+    document.head.children
+        .add(new ScriptElement()..src = 'https://www.youtube.com/iframe_api');
     context['onYouTubeIframeAPIReady'] = _onAPIReady;
+
     apiLoaded = true;
   }
 
   @override
-  void ngOnChanges(Map<String, SimpleChange> changes)
-  {
-    if (changes.containsKey('videoId') && _player != null)
-    {
+  void ngOnChanges(Map<String, SimpleChange> changes) {
+    if (changes.containsKey('videoId') && _player != null) {
       _player.callMethod('cueVideoById', [videoId]);
     }
   }
 
   @override
-  void ngOnDestroy()
-  {
+  void ngOnDestroy() {
     _onStateChangeController.close();
   }
 
-  void _onAPIReady()
-  {
-    /**
-     * Youtube API is ready, initialize video
-     */
+  void _onAPIReady() {
+    // Youtube API is ready, initialize video
     _player = new JsObject(context['YT']['Player'], [elementId, params]);
   }
 
-  void _onReady(JsObject event)
-  {
-  }
+  void _onReady(JsObject event) {}
 
-  void _onStateChange(JsObject event)
-  {
-    switch (event['data'])
-    {
+  void _onStateChange(JsObject event) {
+    switch (event['data']) {
       case -1:
         _onStateChangeController.add('Start');
+        playing = true;
         break;
 
       case 0:
         _onStateChangeController.add('End');
+        playing = false;
         break;
 
       case 1:
         _onStateChangeController.add('Play');
+        playing = true;
         break;
 
       case 2:
         _onStateChangeController.add('Pause');
+        playing = false;
         break;
 
       case 3:
@@ -80,16 +84,16 @@ class FoYouTubePlayerComponent implements AfterContentInit, OnChanges, OnDestroy
     }
   }
 
-  JsObject get params
-  {
+  JsObject get params {
     final vars = <String, String>{};
     vars['fs'] = '1';
-    vars['rel'] = '0';                // Show related videos at the end of playback
-    vars['modestbranding'] = '0';     // Show minimal youtube branding
+    vars['rel'] = '0'; // Show related videos at the end of playback
+    vars['modestbranding'] = '0'; // Show minimal youtube branding
     vars['showinfo'] = '1';
     vars['origin'] = Uri.base.origin;
     vars['enablejsapi'] = '1';
     vars['autoplay'] = autoplay ? '1' : '0';
+    vars['controls'] = '0';
     final events = <String, YoutubeCallback>{};
     events['onReady'] = _onReady;
     events['onStateChange'] = _onStateChange;
@@ -101,7 +105,8 @@ class FoYouTubePlayerComponent implements AfterContentInit, OnChanges, OnDestroy
     return new JsObject.jsify(params);
   }
 
-  final StreamController<String> _onStateChangeController = new StreamController();
+  final StreamController<String> _onStateChangeController =
+      new StreamController();
 
   JsObject _player;
 
@@ -116,5 +121,6 @@ class FoYouTubePlayerComponent implements AfterContentInit, OnChanges, OnDestroy
   @Output('stateChange')
   Stream<String> get stateChangeOutput => _onStateChangeController.stream;
 
-  static bool apiLoaded = false;
+  static bool apiLoaded = false;  
+  bool playing = false;
 }
